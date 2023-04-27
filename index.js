04dd4e5e2be9191afa8c6985f408db3ce9f3ce48
jeffersonmacedocarvalho <dirtyhumantrash@gmail.com>
2023-04-27 08:53:25 -0300

    exercicios

---
+++ index.js
@@ -0,0 +1,83 @@
+// console.log("hello")
+// //comentario
+// //prompt("digite seu nome:")
+
+// const nome = prompt("informe seu nome:")
+
+// const idade = prompt(nome + " informe sua idade")
+
+// const endereco = prompt("infrome seu rua:")
+
+// console.log("seu nome é:" + nome)
+// console.log("você tem:" + idade + " anos de idade")
+// console.log("e mora na rua:" + endereco)
+
+//-------------------------------------------------------------------
+
+// console.log("olá estudante.");
+
+// const nome = prompt("informe seu nome:")
+
+// const SB_nome = prompt(nome + " agora informe seu segundo nome:")
+
+// const idade = prompt("ok. informe sua idade:")
+
+// const estudante = prompt(nome + " " + SB_nome + " você estuda? se sim qual ano?")
+
+// console.log("seu nome é:" + nome);
+// console.log("com o sobre nome:" + SB_nome);
+// console.log("você tem:" + idade + " de idade");
+// console.log("seu grau de estudo é:" + estudante);
+
+//---------------------------------------------------------------------
+
+//const idade = Number (prompt("informe sua idade"))
+
+//console.log(typeof idade);
+
+//--------------------------------------------------------------------
+
+// let p = prompt("Quantas horas você trabalha por dia?")
+// let t = prompt("Quanto você recebe por dia?")
+// alert(`Voce recebe ${t/p} por hora`)
+
+//--------------------------------------------------------------------
+
+// let nome  
+// let idade 
+
+// console.log(typeof nome);
+// console.log(typeof idade);
+// //esse tipo de varivel foi impresso pois dei um comando para que ela aparecesse na tela
+// //e ela esta com este tipo pois não atribui nenhum valor a ela
+
+// nome = String(prompt("informe seu nome:"))
+// idade = Number(prompt("informe sua idade:"))
+
+// console.log(typeof nome);
+// console.log(typeof idade);
+
+// console.log("seu nome é:" + nome)
+// console.log(nome + " sua idade é:" + idade)
+
+//este é um codigo que cria variaveis sem valores de inicio e no meio do codigo
+//colocamos valores nelas e definindo o tipo delas, por fim mostra tudo na tela
+
+//---------------------------------------------------------------------
+
+// let a 
+// let b
+// let c 
+
+// alert("informe os novos valores");
+// a = Number(prompt("informe um valor:"))
+// b = Number(prompt("informe um segundo valor:"))
+
+// c = a
+// a = b
+// b = c
+
+// console.log("novo valor do primeiro numero:" + a);
+// console.log("novo valor do segundo numero:" + b)
+
+//------------------------------------------------------------------------
